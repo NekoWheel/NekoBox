aca5496ea19f3d11db272584c06537c1e61c3be1
E99p1ant <i@github.red>
2023-03-23 02:39:43 +0800

    route: check phone and verify code

--- route/auth/register.go
+++ route/auth/register.go
@@ -5,6 +5,11 @@
 package auth
 
 import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/flamego/cache"
 	"github.com/flamego/recaptcha"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -14,17 +19,50 @@ import (
 	"github.com/NekoWheel/NekoBox/internal/db"
 	"github.com/NekoWheel/NekoBox/internal/form"
 	"github.com/NekoWheel/NekoBox/internal/security/sms"
+	"github.com/NekoWheel/NekoBox/internal/strutil"
 )
 
 func Register(ctx context.Context) {
 	ctx.Success("auth/register")
 }
 
-func SendRegisterSMS(ctx context.Context, f form.RegisterSendSMS, sms sms.SMS) {
+const (
+	smsCodeCacheKeyPrefix = "register-sms-code:"
+)
 
+func SendRegisterSMS(ctx context.Context, f form.RegisterSendSMS, sms sms.SMS, cache cache.Cache, recaptcha recaptcha.RecaptchaV3) {
+	// Check recaptcha code.
+	resp, err := recaptcha.Verify(f.Recaptcha, ctx.Request().Request.RemoteAddr)
+	if err != nil {
+		logrus.WithContext(ctx.Request().Context()).WithError(err).Error("Failed to check recaptcha")
+		_ = ctx.ServerError()
+		return
+	}
+	if !resp.Success {
+		_ = ctx.JSONError(http.StatusBadRequest, "验证码错误")
+		return
+	}
+
+	phone := f.Phone
+	code := strutil.RandomNumericString(6)
+	smsCodeCacheKey := smsCodeCacheKeyPrefix + phone
+	if err := cache.Set(ctx.Request().Context(), smsCodeCacheKey, code, 5*time.Minute); err != nil {
+		logrus.WithContext(ctx.Request().Context()).WithError(err).Error("Failed to set sms code cache")
+		_ = ctx.ServerError()
+		return
+	}
+
+	if err := sms.SendCode(ctx.Request().Context(), phone, code); err != nil {
+		logrus.WithContext(ctx.Request().Context()).WithError(err).Error("Failed to send sms code")
+		_ = ctx.JSONError(http.StatusBadRequest, "发送短信验证码失败，请稍后重试")
+		return
+	}
+
+	logrus.WithContext(ctx.Request().Context()).WithField("phone", phone).WithField("code", code).Info("Send sms code successfully")
+	_ = ctx.JSON("发送短信验证码成功")
 }
 
-func RegisterAction(ctx context.Context, f form.Register, recaptcha recaptcha.RecaptchaV3) {
+func RegisterAction(ctx context.Context, f form.Register, cache cache.Cache, recaptcha recaptcha.RecaptchaV3) {
 	if ctx.HasError() {
 		ctx.Success("auth/register")
 		return
@@ -44,6 +82,27 @@ func RegisterAction(ctx context.Context, f form.Register, recaptcha recaptcha.Re
 		return
 	}
 
+	verifyType := db.VerifyTypeUnverified
+	// Check sms code.
+	if f.Phone != "" && f.VerifyCode != "" {
+		verifyCodeInf, err := cache.Get(ctx.Request().Context(), smsCodeCacheKeyPrefix+f.Phone)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				ctx.SetErrorFlash("验证码已过期")
+			} else {
+				logrus.WithContext(ctx.Request().Context()).WithError(err).Error("Failed to read password recovery code cache")
+				ctx.SetInternalErrorFlash()
+			}
+			ctx.Redirect("/register")
+			return
+		} else {
+			verifyCode, ok := verifyCodeInf.(string)
+			if ok && verifyCode != "" && verifyCode == f.VerifyCode {
+				verifyType = db.VerifyTypeVerified
+			}
+		}
+	}
+
 	if err := db.Users.Create(ctx.Request().Context(), db.CreateUserOptions{
 		Name:       f.Name,
 		Password:   f.Password,
@@ -52,6 +111,7 @@ func RegisterAction(ctx context.Context, f form.Register, recaptcha recaptcha.Re
 		Domain:     f.Domain,
 		Background: conf.Upload.DefaultBackground,
 		Intro:      "问你想问的",
+		VerifyType: verifyType,
 	}); err != nil {
 		switch {
 		case errors.Is(err, db.ErrUserNotExists),
