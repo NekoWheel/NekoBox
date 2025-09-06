80d96449d3e6ddfe4784145f63d944c527031e73
E99p1ant <i@github.red>
2025-09-07 02:26:05 +0800

    web: 404 page
    Signed-off-by: E99p1ant <i@github.red>

--- web/src/pages/404.vue
+++ web/src/pages/404.vue
@@ -1,11 +1,39 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <div class="uk-section uk-flex uk-flex-center uk-flex-middle">
+    <div class="uk-container uk-container-small uk-text-center">
+      <h1 class="error-code uk-margin-remove">404</h1>
 
+      <h2 class="uk-margin-small">页面未找到</h2>
+
+      <div class="uk-margin-large">
+        <button @click="handleBack" type="button" href="/"
+                class="uk-button uk-button-primary uk-button-large uk-margin-small">
+          返回主页
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import {useRouter} from "vue-router";
 
+const router = useRouter()
+const handleBack = () => {
+  router.push({name: 'home'})
+}
+</script>
+
+<style scoped>
+.error-code {
+  font-size: 8rem;
+  font-weight: 200;
+  line-height: 1;
+}
+
+@media (max-width: 640px) {
+  .error-code {
+    font-size: 5rem;
+  }
+}
 </style>
