7bc6e46b83a8048e1fcb2f061fff4369cda1d8e9
E99p1ant <i@github.red>
2025-09-07 03:09:20 +0800

    web: update vite config
    Signed-off-by: E99p1ant <i@github.red>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,4 +25,15 @@ export default defineConfig({
             },
         }
     },
+
+    build: {
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[hash].js',
+                chunkFileNames: 'assets/[hash].js',
+                assetFileNames: 'assets/[hash].[ext]',
+            },
+        },
+    }
 })
